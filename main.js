b1983401767ea4a5ae93aae21501b35bd78a15d5
Petra Bočková <Wickky@seznam.cz>
2022-02-15 18:51:47 +0100

    using the classList and add/remove class function

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 // tady je místo pro náš program
 
 
-console.log("ahoj");
+
+
+let nadpis = document.querySelector('h1');
+// přidáme vlastnosti třídy pomocí class.List.add a do závorky napíšeme název css třídy
+nadpis.classList.add('zluty');
+
+// odebrání určité třídy:
+nadpis.classList.remove('zluty');
+
+// přidání nové třídy div:
+let ctverecek = document.querySelector('div.zluty');
+
+ctverecek.classList.add('ctverecek');
+
+//funkce toggle nám přepíná mezi zapnutí třídy a vypnutí třídy, tzn. pokud tam třída je, tak ji odebere, pokud tam není, tak ji přidá
